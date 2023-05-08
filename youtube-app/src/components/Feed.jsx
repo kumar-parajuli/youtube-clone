f4131abf8db26a8a98951861960bd5706d91d1a8
parajuli rahul <parajulirahul98@gmail.com>
2023-05-08 22:59:41 +0545

    add contained on code

--- youtube-app/src/components/Feed.jsx
+++ youtube-app/src/components/Feed.jsx
@@ -1,18 +1,36 @@
-import React from "react";
+import { useState, useEffect } from "react";
 import { Box, Stack, Typography } from "@mui/material";
 import Sidebar from "./Sidebar";
+import Videos from "./Videos";
+
+import { fetchFromAPI } from "../utils/fetchFromAPI";
 
 const Feed = () => {
+  const [selectedCategory, setSelectedCategory] = useState("New");
+  const [videos, setVideos] = useState(null);
+
+
+  useEffect(() => {
+    setVideos(null);
+
+    fetchFromAPI(`search?part=snippet&q=${selectedCategory}`)
+    
+    .then((data) => setVideos(data.items))
+  }, [selectedCategory]);
   return (
     <Stack sx={{ flexDirection: { sx: "column", md: "row" } }}>
       <Box
         sx={{
-          height: { sx: "auto", md: "92vh" },
+          height: { sx: "auto", md: "100%" },
           borderRight: "1px solid #3d3d3d",
           px: { sx: 0, md: 2 },
         }}
       >
-        <Sidebar />
+        <Sidebar 
+        selectedCategory={selectedCategory} setSelectedCategory={setSelectedCategory}
+
+
+        />
         <Typography
           className="copyright"
           variant="body2"
@@ -20,6 +38,17 @@ const Feed = () => {
         >
           Copy right 2022
         </Typography>
+      </Box>
+      <Box p={2} sx={{ overflow: "auto", height: "90vh", flex: 2 }}>
+        <Typography
+          variant="h4"
+          fontweight="bold"
+          mb={2}
+          sx={{ color: "white" }}
+        >
+          New <span style={{ color: "#f31503" }}>videos</span>
+        </Typography>
+        <Videos videos={videos} />
       </Box>
     </Stack>
   );
